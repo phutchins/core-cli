03d5fe53f9f45f2b4def33b6fb91b0fb7800443f
aleitner <leitnersalex@gmail.com>
2016-09-27 11:11:43 -0400

    Refactoring download script

--- bin/actions/downloader.js
+++ bin/actions/downloader.js
@@ -6,6 +6,7 @@ var path = require('path');
 var storj = require('storj-lib');
 var assert = require('assert');
 var async = require('async');
+var through = require('through');
 
 function Downloader(client, keypass, options) {
   if (!(this instanceof Downloader)) {
@@ -17,7 +18,8 @@ function Downloader(client, keypass, options) {
   this.filepath = options.filepath;
   this.client = client();
   this.keypass = keypass();
-  var self = this;
+  this.fileMeta = null;
+  this.exclude = options.env.exclude;
 
   this._validate();
 }
@@ -49,7 +51,6 @@ Downloader.prototype._validate = function() {
 
 Downloader.prototype._getInfo = function(callback) {
   var self = this;
-  var fileMatch = null;
 
   this.client.listFilesInBucket(this.bucket, function(err, files) {
     if (err) {
@@ -63,16 +64,16 @@ Downloader.prototype._getInfo = function(callback) {
           'Name: %s, Type: %s, Size: %s bytes, ID: %s',
           [file.filename, file.mimetype, file.size, file.id]
         );
-        fileMatch = file;
+        self.fileMeta = file;
       }
     });
 
-    return callback(fileMatch);
+    return callback(null);
   });
 };
 
-Downloader.prototype._determineSaveLocation = function(file, callback) {
-  if (file === null) {
+Downloader.prototype._determineSaveLocation = function(callback) {
+  if (this.fileMeta === null) {
     callback(
       new Error(
         'file ' + this.fileid + ' does not exist in bucket ' + this.bucket
@@ -82,10 +83,10 @@ Downloader.prototype._determineSaveLocation = function(file, callback) {
 
   if (storj.utils.existsSync(this.filepath)) {
     // Check if given path is a directory
-    if (fs.statSync(this.filepath).isDirectory() && file !== null) {
+    if (fs.statSync(this.filepath).isDirectory() && this.file !== null) {
 
       // use the file name as the name of the file to be downloaded to
-      var fullpath = path.join(this.filepath,file.filename);
+      var fullpath = path.join(this.filepath,this.fileMeta.filename);
 
       // Make sure fullpath doesn't already exist
       if (storj.utils.existsSync(fullpath)) {
@@ -101,7 +102,6 @@ Downloader.prototype._determineSaveLocation = function(file, callback) {
   } else {
     this.destination = this.filepath;
   }
-
   callback(null);
 };
 
@@ -119,93 +119,86 @@ Downloader.prototype._getKeyRing = function(callback) {
   });
 };
 
+/**
+ * set this.keyring using this.keypass
+ * @private
+ */
+Downloader.prototype._createFileStream = function(callback) {
+  var self = this;
+  this.target = fs.createWriteStream(this.destination);
+
+  this.target.on('finish', function() {
+    log('info', 'File downloaded and written to %s.', [self.destination]);
+  }).on('error', function(err) {
+    callback(err);
+  });
+
+  this.client.createFileStream(
+    this.bucket,
+    this.fileid,
+    {exclude: this.exclude.split(',')},
+    callback
+  );
+};
+
+Downloader.prototype._handleFileStream = function(stream, callback) {
+  var self = this;
+  var received = 0;
+  var secret = this.keyring.get(this.fileid);
+
+  if (!secret) {
+    return log('error', 'No decryption key found in key ring!');
+  }
+
+  var decrypter = new storj.DecryptStream(secret);
+
+  stream.on('error', function(err) {
+    log('warn', 'Failed to download shard, reason: %s', [err.message]);
+    fs.unlink(this.destination, function(unlinkFailed) {
+      if (unlinkFailed) {
+        callback(new Error('Failed to unlink partial file.'));
+      }
+
+      if (!err.pointer) {
+        return;
+      }
+
+      log('info', 'Retrying download from other mirrors...');
+      var exclude = this.exclude.split(',');
+      exclude.push(err.pointer.farmer.nodeID);
+      self.start(this.finalCallback);
+    });
+  }).pipe(through(function(chunk) {
+    received += chunk.length;
+    log('info', 'Received %s of %s bytes', [received, stream._length]);
+    this.queue(chunk);
+  })).pipe(decrypter).pipe(this.target);
+};
 
 Downloader.prototype.start = function(finalCallback) {
   var self = this;
+  this.finalCallback = finalCallback;
 
   async.waterfall([
     function _getInfo(callback) {
       self._getInfo(callback);
     },
-    function _determineSaveLocation(file, callback) {
-      self._determineSaveLocation(file, callback);
+    function _determineSaveLocation(callback) {
+      self._determineSaveLocation(callback);
     },
     function _getKeyRing(callback) {
       self._getKeyRing(callback);
+    },
+    function _createFileStream(callback) {
+      self._createFileStream(callback);
+    },
+    function _handleFileStream(stream, callback) {
+      self._handleFileStream(stream, callback);
     }
   ], function (err, filepath) {
     finalCallback(err, filepath);
   });
 
 };
-
-// module.exports.download = function(bucket, id, filepath, env) {
-//   var self = this;
-//   var destination = filepath;
-//
-//
-//
-//   module.exports.getInfo.call(self, bucket, id, function(file) {
-//     var target;
-//
-//
-//
-//     // Check if path is an existing path
-
-//     utils.getKeyRing(keypass, function(keyring) {
-//       var secret = keyring.get(id);
-//
-//       if (!secret) {
-//         return log('error', 'No decryption key found in key ring!');
-//       }
-//
-//       var decrypter = new storj.DecryptStream(secret);
-//       var received = 0;
-//       var exclude = env.exclude.split(',');
-//
-//       target.on('finish', function() {
-//         log('info', 'File downloaded and written to %s.', [destination]);
-//       }).on('error', function(err) {
-//         log('error', err.message);
-//       });
-//
-//       client.createFileStream(bucket, id, {
-//         exclude: exclude
-//       },function(err, stream) {
-//         if (err) {
-//           return log('error', err.message);
-//         }
-//
-//         stream.on('error', function(err) {
-//           log('warn', 'Failed to download shard, reason: %s', [err.message]);
-//           fs.unlink(filepath, function(unlinkFailed) {
-//             if (unlinkFailed) {
-//               return log('error', 'Failed to unlink partial file.');
-//             }
-//
-//             if (!err.pointer) {
-//               return;
-//             }
-//
-//             log('info', 'Retrying download from other mirrors...');
-//             exclude.push(err.pointer.farmer.nodeID);
-//             module.exports.download.call(
-//               self,
-//               bucket,
-//               id,
-//               filepath,
-//               { exclude: env.exclude.join(',')}
-//             );
-//           });
-//         }).pipe(through(function(chunk) {
-//           received += chunk.length;
-//           log('info', 'Received %s of %s bytes', [received, stream._length]);
-//           this.queue(chunk);
-//         })).pipe(decrypter).pipe(target);
-//       });
-//     });
-//   });
-//
-// };
 
 module.exports = Downloader;
